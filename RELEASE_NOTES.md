d59efad0acff17116bf2abf7899d41073397aa76
Guy Oliver <guy.oliver@gmail.com>
2016-05-08 20:14:55 -0400

    updated version, remove fsharp.core

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.0.2 - May 8 2016
+* Removed dependancy on fsharp.core
+
 #### 0.0.1 - May 8 2016
 * Initial Release
  
--- src/FSharp.AsyncBuilderExtensions/AssemblyInfo.fs
+++ src/FSharp.AsyncBuilderExtensions/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.AsyncBuilderExtensions")>]
 [<assembly: AssemblyProductAttribute("FSharp.AsyncBuilderExtensions")>]
 [<assembly: AssemblyDescriptionAttribute("Extenions to the AsyncBuilder to allow Tasks to be used in an async block")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
-    let [<Literal>] InformationalVersion = "0.0.1"
+    let [<Literal>] Version = "0.0.2"
+    let [<Literal>] InformationalVersion = "0.0.2"
